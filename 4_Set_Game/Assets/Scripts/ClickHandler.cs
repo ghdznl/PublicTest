1773e37b79e61fdd2c5c6bc2f668a1a49b400268
JY4 <nayuta478@gmail.com>
2022-02-04 17:23:09 +0900

    Revert "fsafsedgg"
    This reverts commit 4dfd1de9dacf6992a456cb4a8974b661f358d433.

--- 4_Set_Game/Assets/Scripts/ClickHandler.cs
+++ 4_Set_Game/Assets/Scripts/ClickHandler.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class ClickHandler : MonoBehaviour
 {
-    //is mouse button up in the last frame?fdhfdh
+    //is mouse button up in the last frame?
     bool mouseUp = true;
 
     public GameObject Effect;
